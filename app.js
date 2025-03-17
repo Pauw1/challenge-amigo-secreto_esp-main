fc6cb7f77f5e481e065615f53ffcb5f29f33bb9b
Paulina Rojas <rojasgpaulina@gmail.com>
2025-03-16 23:32:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let amigo = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if(!amigo)  {
+        alert("Ingresa un nombre.");
+        return;
+    }
+    amigo.push(nombreAmigo)
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    renderizarlistaAmigos();
+    console.log(amigo);
+}
+
+function renderizarlistaAmigos(){
+    let listaAmigos= document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigo.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if(amigo.length < 3) {
+        alert("Necesitas ingresar por lo menos 3 amigos para hacer un sorteo.");
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Tu amigo secreto es: ${amigoSorteado}`;
+    
+}
